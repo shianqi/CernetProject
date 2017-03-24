90c6bc56d5c79c86b76a175ee346af30e36ac17d
killer <shianqi@imudges.com>
2017-03-24 20:48:15 +0800

    前端登陆界面完成，后端链接消息完成

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,54 @@
 /**
  * Created by killer on 2017/3/17.
  */
-var submitMessage = function(){
-	var chatInput = document.getElementById('chatBottomInputText');
-	var chatMessage = chatInput.value;
-	console.log(chatMessage);
 
-	var chatMain = document.getElementsByClassName("chatMain");
-	var newMessage = document.createElement("div");
-	var message = document.createElement("p");
-	message.innerHTML = chatMessage;
+var WS_URL = "183.175.12.160:9988" + "/CloudCourse" + "/websocket";
+if (location.protocol == 'http:') {
+	WS_URL = "ws://"+WS_URL;
+} else { // 如果页面是https,那么必须走wss协议
+	WS_URL = "wss://"+WS_URL;
+}
+var webSocket = new WebSocket(WS_URL);
+window.onload = function () {
+	webSocket.send('{"action": "in_course","course_id":1}');
+	webSocket.onerror = function(event) {
+		//
+	};
 
-	//为div创建属性class = "test"
-	var chatMessageClass = document.createAttribute("class");
-	chatMessageClass.value = "chatMessage";
-
-	//把属性class = "test"添加到div
-	newMessage.setAttributeNode(chatMessageClass);
-	newMessage.appendChild(message);
-	chatMain[0].appendChild(newMessage);
-	chatInput.value = "";
-	return false;
+	webSocket.onopen = function(event) {
+		//
+	};
 };
+
+
+var ChatBox = new Vue({
+	el: '#chatBox',
+	data: {
+		messageList : [],
+		message: '',
+	},
+	methods: {
+		nickname: function (value) {
+			if(value.user && value.user.realName){
+				return value.user.realName;
+			}else{
+				return '游客';
+			}
+		},
+		sendMessage: function(event){
+			webSocket.send('{"action":"send_msg","course_id":1,"msg":"' + this.message + '","video_time":3}');
+			this.message = '';
+			return false;
+		}
+	},
+	mounted: function () {
+		webSocket.onmessage = function (event) {
+			var eventJson = JSON.parse(event.data);
+			this.messageList.push(eventJson);
+		}.bind(this);
+	}
+});
+
+
+
+
